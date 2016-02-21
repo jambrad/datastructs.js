98e5d738de3878a369333e6532eb1cb013631793
Arnelle Balane <arnellebalane@gmail.com>
2016-02-21 15:10:47 +0800

    update test linkedlistnode test cases to make use of testing context

--- source/linked-list/tests/linked-list.js
+++ source/linked-list/tests/linked-list.js
@@ -5,34 +5,34 @@ import LinkedListNode from '../linked-list-node';
 
 describe('LinkedList', () => {
     describe('.add(item)', () => {
-        let list = null;
-
-        beforeEach(() => list = new LinkedList());
-
-        it('should set head to first item', () => {
-            list.add(1);
-            assert.equal(list._head.value, 1);
+        beforeEach(function() {
+            this.list = new LinkedList();
         });
 
-        it('should point tail to the latest item', () => {
-            list.add(1);
-            assert.equal(list._tail.value, 1);
-
-            list.add(2);
-            assert.equal(list._tail.value, 2);
+        it('should set head to first item', function() {
+            this.list.add(1);
+            assert.equal(this.list._head.value, 1);
         });
 
-        it('should initialize LinkedListNode for added item', () => {
-            list.add(1);
-            assert.ok(list._head instanceof LinkedListNode);
+        it('should point tail to the latest item', function() {
+            this.list.add(1);
+            assert.equal(this.list._tail.value, 1);
+
+            this.list.add(2);
+            assert.equal(this.list._tail.value, 2);
         });
 
-        it('should increment list length', () => {
-            list.add(1);
-            assert.equal(list.length, 1);
+        it('should initialize LinkedListNode for added item', function() {
+            this.list.add(1);
+            assert.ok(this.list._head instanceof LinkedListNode);
+        });
 
-            list.add(2);
-            assert.equal(list.length, 2);
+        it('should increment list length', function() {
+            this.list.add(1);
+            assert.equal(this.list.length, 1);
+
+            this.list.add(2);
+            assert.equal(this.list.length, 2);
         });
     });
 });
